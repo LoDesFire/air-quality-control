1b95605467a5ee8967b3aa567706a35f775e10d2
Daniil Shumskiy <dan200412@gmail.com>
2023-11-19 04:05:50 +0300

    Not bad version. I like it :)

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-#include <cstring>
+// #include <cstring>
 
 #include <MQUnifiedsensor.h>
 #include <DHT.h>
@@ -16,23 +16,19 @@
 #define DHT_PIN 18
 #define MQ135_PIN 35
 #define MQ4_PIN 34
+#define CH4_ISR_PIN 19
 #define ONE_WIRE_PIN 15
-#define PERIOD_SEC 5
+
+#define COLLECT_PERIOD_SEC 5
 #define ISSUE_TIMER_SEC 600
+#define RECORDS_PACKAGE_SIZE 3
+#define RECORDS_QUEUE_SIZE 5
+#define EXTRA_QUEUE_SIZE 5
 
-static void recordingsTimerCallback(void *args);
-
-esp_timer_handle_t _timer = NULL;
-
-esp_timer_create_args_t timerConfig = {
-    .callback = recordingsTimerCallback,
-    .arg = NULL,
-    .dispatch_method = ESP_TIMER_TASK,
-    .name = "get_recordings_timer",
-    .skip_unhandled_events = true};
+#define RECORDS_ITEM_SIZE sizeof(char *)
+#define EXTRA_ITEM_SIZE sizeof(int *)
 
 esp_timer_handle_t restart_timer = NULL;
-
 esp_timer_create_args_t restartTimerConfig = {
     .callback = [](void *)
     { ESP.restart(); },
@@ -40,6 +36,11 @@ esp_timer_create_args_t restartTimerConfig = {
     .dispatch_method = ESP_TIMER_TASK,
     .name = "restart_timer",
     .skip_unhandled_events = false};
+
+StaticQueue_t _recordsQueueBuffer;
+uint8_t _recordsQueueStorage[RECORDS_QUEUE_SIZE * RECORDS_ITEM_SIZE];
+StaticQueue_t _extraQueueBuffer;
+uint8_t _extraQueueStorage[EXTRA_QUEUE_SIZE * EXTRA_ITEM_SIZE];
 
 // const char *ssid = "define (;;) ever \\n forever";
 // const char *password = "bsuir_the_best";
@@ -64,102 +65,10 @@ enum EspState
 };
 
 int espState = EspState::NONE;
-TaskHandle_t pushQueryTaskHandle;
-TaskHandle_t pushExtraQueryTaskHandle;
-
-class ServerApi
-{
-  String apiUrl;
-  DynamicJsonDocument *records;
-  StaticJsonDocument<20> **statusCodes;
-  HTTPClient *httpClient;
-  const int STATUS_CODES_LENGTH = 10;
-  int statusCodesPointer;
-
-  int POST(String endpoint, JsonDocument *document)
-  {
-    String payload;
-    httpClient = new HTTPClient();
-    httpClient->begin(apiUrl + endpoint);
-    httpClient->addHeader("Content-Type", "application/json");
-    serializeJson(*document, payload);
-    vTaskDelay(pdMS_TO_TICKS(1));
-    int code = httpClient->POST(payload);
-    delete httpClient;
-
-    ESP_LOGI("api", "POST query. Response code: %d", code);
-    return code;
-  }
-
-public:
-  ServerApi()
-  {
-    apiUrl = "https://airqualityapi-georga399.amvera.io/api/";
-    records = new DynamicJsonDocument(40000);
-    statusCodes = new StaticJsonDocument<20> *[STATUS_CODES_LENGTH]
-    { nullptr };
-    statusCodesPointer = -1;
-  }
-
-  void push(uint32_t timestamp, float *raw)
-  {
-    JsonObject record = records->createNestedObject();
-
-    record["date"] = timestamp;
-    record["temperatureC"] = raw[0];
-    record["humidity"] = raw[1];
-    record["cH4"] = raw[2];
-    record["cO2"] = raw[3];
-
-    ESP_LOGI("api", "Pushed new record. Size: %d", records->size());
-  }
-
-  void pushExtra(int *extraCode)
-  {
-    auto extraData = new StaticJsonDocument<20>;
-    // int *code = static_cast<int *>(extraCode);
-    (*extraData)["statusCode"] = *extraCode;
-    delete extraCode;
-    if (statusCodesPointer < STATUS_CODES_LENGTH - 1)
-      statusCodes[++statusCodesPointer] = extraData;
-    // makeQueryTask();
-  }
-
-  void pushQuery(void *)
-  {
-    // DynamicJsonDocument *records = static_cast<DynamicJsonDocument *>(jsonDocument);
-    int responseCode = 0;
-    ESP_LOGI("tasks", "%d", esp_get_free_heap_size());
-    int responseCode = POST("rawdata/push", records);
-    if (responseCode == 200 || responseCode == -11)
-    {
-      delete records;
-      records = new DynamicJsonDocument(40000);
-    }
-  }
-
-  void pushExtraQuery(void *)
-  {
-    // StaticJsonDocument<20> *statusCode = static_cast<StaticJsonDocument<20> *>(jsonDocument);
-    int responseCode = POST("rawdata/pushExtra", statusCodes[statusCodesPointer]);
-
-    if (responseCode == 200 || responseCode == -11)
-    {
-      delete statusCodes[statusCodesPointer];
-      statusCodesPointer--;
-    }
-  }
-
-  bool isExtraEmpty()
-  {
-    return statusCodesPointer != -1;
-  }
-
-  int recordsSize()
-  {
-    return records->size();
-  }
-};
+TaskHandle_t apiTask = NULL;
+TaskHandle_t collectorTask = NULL;
+QueueHandle_t recordsQueue;
+QueueHandle_t extraQueue;
 
 class Sensors
 {
@@ -222,103 +131,20 @@ public:
   }
 };
 
-ServerApi api;
-Sensors sensors(MQ4_PIN, MQ135_PIN, DHT_PIN, ONE_WIRE_PIN);
-
-void pushExtraQuery(void *)
+static IRAM_ATTR void highCH4Callback()
 {
-  api.pushExtraQuery(nullptr);
-  vTaskDelete(NULL);
-}
-
-void pushQuery(void *)
-{
-  api.pushQuery(nullptr);
-  vTaskDelete(NULL);
-}
-
-void pushExtra(void *code)
-{
-  int *extraCode = static_cast<int *>(code);
-  api.pushExtra(extraCode);
-  vTaskDelete(NULL);
-}
-
-void makeQueryTask()
-{
-  if (api.isExtraEmpty())
-  {
-    xTaskCreate(
-        pushExtraQuery,
-        "pushExtraQuery",
-        20480,
-        // static_cast<void *>(statusCodes[statusCodesPointer]),
-        nullptr,
-        4 | portPRIVILEGE_BIT,
-        &pushExtraQueryTaskHandle);
-  }
-  if (api.recordsSize() > 0)
-    xTaskCreate(
-        pushQuery,
-        "pushQuery",
-        20480,
-        nullptr,
-        1 | portPRIVILEGE_BIT,
-        &pushQueryTaskHandle);
-}
-
-static void recordingsTimerCallback(void *args)
-{
-  ESP_LOGI("timer", "Callback");
-  tm loctime;
-  getLocalTime(&loctime);
-  float *records = sensors.get_records();
-
-  if (records == nullptr)
-  {
-    ESP_LOGI("recordings", "Timestamp: %u", mktime(&loctime));
-    if (espState != EspState::SENSORS_ISSUE)
-    {
-      int *code = new int(EspState::SENSORS_ISSUE);
-      api.pushExtra(code);
-    }
-  }
-  else
-  {
-    if (espState == EspState::SENSORS_ISSUE)
-      espState = EspState::NONE;
-    ESP_LOGI("recordings", "Timestamp: %u, values: [%f, %f, %f, %f]", mktime(&loctime), records[0], records[1], records[2], records[3]);
-  }
-
-  api.push(mktime(&loctime), records);
-
-  makeQueryTask();
-}
-
-static IRAM_ATTR void highCH4Callback(void *args)
-{
-  TaskHandle_t null;
   int *code = new int(EspState::HIGH_CH4);
-  xTaskCreate(
-      pushExtra,
-      "highCH4",
-      3000,
-      static_cast<void *>(code),
-      4 | portPRIVILEGE_BIT,
-      &null);
+  int ret;
+  xQueueSendFromISR(extraQueue, &code, &ret);
+  portYIELD_FROM_ISR(ret);
 }
 
-static IRAM_ATTR void highCO2Callback(void *args)
+static IRAM_ATTR void highCO2Callback()
 {
-  TaskHandle_t null;
   int *code = new int(EspState::HIGH_CO2);
-  xTaskCreate(
-      pushExtra,
-      "highCO2",
-      3000,
-      static_cast<void *>(code),
-      4 | portPRIVILEGE_BIT,
-      &null);
+  int ret;
+  xQueueSendFromISR(extraQueue, &code, &ret);
+  portYIELD_FROM_ISR(ret);
 }
 
 void wifiDisconnetcedCallback(arduino_event_t *event)
@@ -327,13 +153,14 @@ void wifiDisconnetcedCallback(arduino_event_t *event)
   {
     ESP_LOGI("restart_timer", "Started");
     esp_timer_start_once(restart_timer, ISSUE_TIMER_SEC * 1000000);
+    vTaskSuspend(apiTask);
   }
   WiFi.begin(ssid, password);
   digitalWrite(LED_BUILTIN, LED_VALUE = !LED_VALUE);
-  delay(1000);
+  vTaskDelay(pdMS_TO_TICKS(1000));
 }
 
-void initializeTime()
+void wifiConnetcedCallback(arduino_event_t *event)
 {
   configTime(3 * 3600, 0, "europe.pool.ntp.org");
   tm inTime;
@@ -341,47 +168,179 @@ void initializeTime()
   {
     getLocalTime(&inTime);
   } while (inTime.tm_year < 71);
-}
-
-void wifiConnetcedCallback(arduino_event_t *event)
-{
-  if (!esp_timer_is_active(_timer))
-  {
-    initializeTime();
-    esp_timer_start_periodic(_timer, PERIOD_SEC * 1000000);
-    ESP_LOGI("timer", "Started");
-  }
-  ESP_LOGI("wifi", "Connected");
+  vTaskResume(apiTask);
   digitalWrite(LED_BUILTIN, LED_VALUE = LOW);
   esp_timer_stop(restart_timer);
   ESP_LOGI("restart_timer", "Dropped");
+  ESP_LOGI("wifi", "Connected");
+}
+
+class Api
+{
+  HTTPClient *httpClient;
+  String apiUrl = "https://airqualityapi-georga399.amvera.io/api/";
+
+public:
+  Api()
+  {
+  }
+
+  int POST(String endpoint, char *payload)
+  {
+    httpClient = new HTTPClient();
+    httpClient->begin(apiUrl + endpoint);
+    httpClient->addHeader("Content-Type", "application/json");
+    int code = httpClient->POST((uint8_t *)payload, strlen(payload));
+    delete httpClient;
+
+    ESP_LOGI("api", "POST query (%s). Response code: %d", endpoint.c_str(), code);
+    return code;
+  }
+};
+
+void api(void *)
+{
+  Api server;
+  while (1)
+  {
+    int *code;
+    char *payload;
+    if (uxQueueMessagesWaiting(extraQueue) > 0 && xQueuePeek(extraQueue, &code, portMAX_DELAY))
+    {
+      StaticJsonDocument<20> extraCode;
+      extraCode["statusCode"] = *code;
+      payload = new char[20];
+      serializeJson(extraCode, payload, 20);
+
+      ESP_LOGI("code", "%s", payload);
+
+      int responseCode = server.POST("rawdata/pushExtra", payload);
+      delete payload;
+      if (responseCode == 200 || responseCode == -11)
+      {
+        if (xQueueReceive(extraQueue, &code, pdMS_TO_TICKS(1)) == pdTRUE)
+          ;
+        delete code;
+      }
+      continue;
+    }
+
+    if (xQueuePeek(recordsQueue, &payload, 0))
+    {
+      int responseCode = server.POST("rawdata/push", payload);
+      if (responseCode == 200 || responseCode == -11)
+      {
+        if (xQueueReceive(recordsQueue, &payload, pdMS_TO_TICKS(1)) == pdTRUE)
+          ;
+        delete payload;
+      }
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+class Collector
+{
+  DynamicJsonDocument *records;
+
+public:
+  Collector()
+  {
+    records = new DynamicJsonDocument(10000);
+  }
+
+  void push(time_t timestamp, float *raw_records)
+  {
+    JsonObject record = records->createNestedObject();
+
+    record["date"] = timestamp;
+    record["temperatureC"] = raw_records[0];
+    record["humidity"] = raw_records[1];
+    record["cH4"] = raw_records[2];
+    record["cO2"] = raw_records[3];
+    delete raw_records;
+
+    ESP_LOGI("collector", "Pushed new record. Size: %d", records->size());
+  }
+
+  size_t size()
+  {
+    return records->size();
+  }
+
+  void serialize(char *item)
+  {
+    serializeJson(*records, item, 8500);
+    delete records;
+    records = new DynamicJsonDocument(8500);
+  }
+};
+
+void collector(void *)
+{
+  Sensors sensors(MQ4_PIN, MQ135_PIN, DHT_PIN, ONE_WIRE_PIN);
+  sensors.begin();
+  Collector cltr;
+  while (1)
+  {
+    tm loctime;
+    getLocalTime(&loctime);
+    float *records = sensors.get_records();
+    if (!records)
+    {
+      if (espState == EspState::NONE)
+      {
+        int *code = new int(EspState::SENSORS_ISSUE);
+        xQueueSend(extraQueue, &code, portMAX_DELAY);
+      }
+    }
+    else
+    {
+      if (espState == EspState::SENSORS_ISSUE)
+        espState = EspState::NONE;
+      cltr.push(mktime(&loctime), records);
+      if (cltr.size() >= RECORDS_PACKAGE_SIZE)
+      {
+        char *item = new char[8500];
+        cltr.serialize(item);
+        xQueueSend(recordsQueue, &item, portMAX_DELAY);
+      }
+    }
+    vTaskDelay(pdMS_TO_TICKS(COLLECT_PERIOD_SEC * 1000));
+  }
+  vTaskDelete(NULL);
 }
 
 void setup()
 {
   Serial.begin(9600);
-  sensors.begin();
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(CH4_ISR_PIN, INPUT);
   digitalWrite(LED_BUILTIN, LED_VALUE = HIGH);
 
   ESP_LOGI("timer", "Software timers creating");
-  esp_timer_create(&timerConfig, &_timer);
   esp_timer_create(&restartTimerConfig, &restart_timer);
-  if (_timer == NULL || restart_timer == NULL)
+  if (restart_timer == NULL)
   {
     ESP_LOGI("timer", "Failed to create");
     ESP.restart();
   }
   ESP_LOGI("timer", "Succsessfully created");
 
+  recordsQueue = xQueueCreateStatic(RECORDS_QUEUE_SIZE, RECORDS_ITEM_SIZE, &_recordsQueueStorage[0], &_recordsQueueBuffer);
+  extraQueue = xQueueCreateStatic(EXTRA_QUEUE_SIZE, EXTRA_ITEM_SIZE, &_extraQueueStorage[0], &_extraQueueBuffer);
+
+  xTaskCreate(api, "API", 10480, nullptr, NULL, &apiTask);
+  xTaskCreate(collector, "Collector", 10480, nullptr, 5, &collectorTask);
+
   ESP_LOGI("wifi", "WiFi connecting...");
   WiFi.onEvent(wifiConnetcedCallback, ARDUINO_EVENT_WIFI_STA_CONNECTED);
   WiFi.onEvent(wifiDisconnetcedCallback, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
   WiFi.onEvent(wifiDisconnetcedCallback, ARDUINO_EVENT_WIFI_STA_LOST_IP);
-
   wifiDisconnetcedCallback(nullptr);
+
+  attachInterrupt(digitalPinToInterrupt(CH4_ISR_PIN), highCH4Callback, RISING);
+  // attachInterrupt(19, highCH4Callback, RISING);  
 }
 
-void loop()
-{
-}
+void loop() {}
